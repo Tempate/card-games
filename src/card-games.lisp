ead43dd55aeb52ce5782eb775048294887d1dae4
Tempate <ddiazq28@gmail.com>
2020-03-12 16:51:22 +0100

    Function to choose between games

--- src/card-games.lisp
+++ src/card-games.lisp
@@ -2,4 +2,15 @@
 
 (in-package #:card-games)
 
-(defun play)
+(defun choose-game (games)
+  (format t "What game do you want to play?~%")
+  (loop :for game :in games
+        :for i :from 0
+        :do (format t "~D. ~A~%" i game))
+  (let ((user-input (read)) (games-len (length games)))
+    (loop :while (and (not (typep user-input 'integer)) (>= user-input 0) (< user-input games-len)) :do
+          (print "Your choice must be a number")
+          (setq user-input (read)))
+    (nth user-input games)))
+
+(defun play ())
